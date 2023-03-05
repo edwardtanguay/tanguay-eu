f608f671180bd4abc630e17bf91d94917ea0cf31
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-05 02:37:52 +0100

    listed initial info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,49 @@ function Welcome() {
 	return (
 		<div className="page pageWelcome">
 			<ul>
-				<li>this site is part of my <a href="https://onespace.pages.dev/forays?id=257">2023 Datapod Projects</a></li>
+				<li>
+					this site is part of my{' '}
+					<a href="https://onespace.pages.dev/forays?id=257">
+						2023 Datapod Projects
+					</a>
+				</li>
 				<li>made with Next.js</li>
+				<li>
+					much of the content for this site is currently available at
+					other sites:
+					<ul>
+						<li>
+							<a href="https://edwardtanguay.vercel.app/howtos">
+								Howtos
+							</a>
+						</li>
+						<li>
+							<a href="https://onespace.pages.dev/forays">
+								Forays
+							</a>
+						</li>
+						<li>
+							<a href="https://onespace.pages.dev/techItems">
+								Tech Items
+							</a>
+						</li>
+						<li>
+							<a href="https://onespace.pages.dev/flashcards">
+								Flashcards
+							</a>
+						</li>
+						<li>
+							<a href="https://jobs.tanguay.eu">
+								Web developer jobs in Germany
+							</a>
+						</li>
+						<li>
+							<a href="https://starters.tanguay.eu">
+								Mern stack starter sites
+							</a>
+						</li>
+					</ul>
+				</li>
 			</ul>
 		</div>
 	);
