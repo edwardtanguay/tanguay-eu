6f0d0120898d4031c5bb1c6fbe2f2c34246e7958
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-29 00:10:53 +0200

    updated about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,11 +3,18 @@ export default function About() {
 	return (
 		<ul>
 			<li className="main">
-				<p className="mb-3">This site is part of my <b>2023 Datapod Project</b> (<a href="https://onespace.pages.dev/forays?id=257">see roadmap</a>) which currently contains these three sites, each which auto-generate their respective framework:</p>
+				<p className="mb-3">
+					This site is part of my <b>2023 Datapod Project</b> (
+					<a href="https://onespace.pages.dev/forays?id=257">
+						see roadmap
+					</a>
+					) which currently contains these three sites, each which
+					auto-generate their respective framework:
+				</p>
 				<ul className="list-disc ml-4">
 					<li>
 						<b>Edward's Tech Site</b> (this site)
-				<ul className="list-disc ml-4">
+						<ul className="list-disc ml-4">
 							<li>
 								<i>
 									my tech site with all my howtos, tech items,
@@ -24,15 +31,15 @@ export default function About() {
 						</ul>
 					</li>
 					<li>
-						<a href="https://datapod-tanguay-eu.vercel.app/about">
-							Datapod Project
+						<a href="https://edward-tanguay-eu.vercel.app/about">
+							Edward Tanguay
 						</a>
-				<ul className="list-disc ml-4">
+						<ul className="list-disc ml-4">
 							<li>
 								<i>
-									landing site for my Datapod project with
-									documentation, current versions, ticket
-									system
+									my personal site: books, languages, online
+									class notes, blog, philosophy, politics,
+									etc.
 								</i>
 							</li>
 							<li>
@@ -45,15 +52,15 @@ export default function About() {
 						</ul>
 					</li>
 					<li>
-						<a href="https://edward-tanguay-eu.vercel.app/about">
-							Edward Tanguay
+						<a href="https://datapod-tanguay-eu.vercel.app/about">
+							Datapod Project
 						</a>
-				<ul className="list-disc ml-4">
+						<ul className="list-disc ml-4">
 							<li>
 								<i>
-									my personal site: books, languages, online
-									class notes, blog, philosophy, politics,
-									etc.
+									landing site for my Datapod project with
+									documentation, current versions, ticket
+									system
 								</i>
 							</li>
 							<li>
