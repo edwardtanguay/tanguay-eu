805e5986dd8169e298965385a5f9c13582324c7a
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-29 13:02:11 +0200

    input basic

--- src/app/howtos/page.tsx
+++ src/app/howtos/page.tsx
@@ -3,7 +3,8 @@ import howtos from '../../data/itemtype_howtos.json';
 export default function About() {
 	return (
 		<>
-			<p className="mb-3">There are {howtos.length} howtos loaded.</p>
+			<p className="text-3xl mb-3">{howtos.length} Howtos</p>
+			<input className='text-3xl placeholder-slate-300 text-slate-500 rounded p-1 mb-3 ' autoFocus placeholder='search howtos'/>
 			{howtos.map((howto) => {
 				return <div key={howto.id}>{howto.title}</div>;
 			})}
