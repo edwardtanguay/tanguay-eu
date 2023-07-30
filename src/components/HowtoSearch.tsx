479850c5692de58516f4a33ac27503ff6b481986
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-30 11:36:36 +0200

    trying useEffect

--- src/components/HowtoSearch.tsx
+++ src/components/HowtoSearch.tsx
@@ -1,27 +1,53 @@
 'use client';
-import { useState } from 'react';
-import _initialHowtos from '../data/itemtype_howtos.json';
+import { useState, useEffect } from 'react';
+import rawHowtos from '../data/itemtype_howtos.json';
 import * as qdat from '../qtools/qdat';
 
-const initialHowtos = _initialHowtos.sort((a, b) => a.systemWhenCreated < b.systemWhenCreated ? 1 : 0);
+interface IHowto {
+	id: number;
+	category: string;
+	title: string;
+	body: string;
+	systemWhenCreated: string;
+}
+
+let initialHowtos: IHowto[] = [];
+for (const rawHowto of rawHowtos) {
+	const howto: IHowto = {
+		id: rawHowto.id,
+		category: rawHowto.category,
+		title: rawHowto.title,
+		body: rawHowto.body,
+		systemWhenCreated: rawHowto.systemWhenCreated
+	};
+	initialHowtos.push(howto);
+}
 
 export default function Howtos() {
 	const [searchText, setSearchText] = useState('');
-	const [howtos, setHowtos] = useState(initialHowtos);
+	const [howtos, setHowtos] = useState<IHowto[]>(initialHowtos);
 
 	const handleSearchTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const _searchText = e.target.value;
 		const titleMatchHowtos = initialHowtos.filter((m) =>
 			m.title.toLowerCase().includes(_searchText.toLowerCase())
 		);
-		const restMatchHowtos = initialHowtos.filter((m) =>
-			m.body.toLowerCase().includes(_searchText.toLowerCase()) && !m.title.toLowerCase().includes(_searchText.toLowerCase())
+		const restMatchHowtos = initialHowtos.filter(
+			(m) =>
+				m.body.toLowerCase().includes(_searchText.toLowerCase()) &&
+				!m.title.toLowerCase().includes(_searchText.toLowerCase())
 		);
 		let _howtos = [...titleMatchHowtos, ...restMatchHowtos];
 
 		setHowtos(_howtos);
 		setSearchText(_searchText);
 	};
+
+	useEffect(() => {
+		initialHowtos = initialHowtos.sort((a, b) =>
+			a.systemWhenCreated < b.systemWhenCreated ? 1 : 0
+		);
+	}, []);
 
 	return (
 		<>
@@ -35,9 +61,14 @@ export default function Howtos() {
 			/>
 			{howtos.map((howto) => {
 				return (
-					<div key={howto.id} className='mb-3'>
-						<div className='text-yellow-400 smallcaps text-sm text-opacity-70'>{qdat.smartDateWithYear(howto.systemWhenCreated)} - {howto.category}</div>
-						<div className='text-slate-50 text-xl'>{howto.title}</div>
+					<div key={howto.id} className="mb-3">
+						<div className="text-yellow-400 smallcaps text-sm text-opacity-70">
+							{qdat.smartDateWithYear(howto.systemWhenCreated)} -{' '}
+							{howto.category}
+						</div>
+						<div className="text-slate-50 text-xl">
+							{howto.title}
+						</div>
 					</div>
 				);
 			})}
