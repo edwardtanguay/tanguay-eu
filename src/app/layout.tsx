691f6b6fe0d110f24170280ddfb09f8de2fd8bdd
Edward Tanguay on dell001 <edward@tanguay.info>
2023-07-30 18:30:42 +0200

    added search highlith

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from '@/components/Header'
-import './globals.css'
+import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const appVersion = () => {
-	return '0.025 fixed sort';
+	return '0.026 search highlight';
 }
