d58075e53f135ac97153379e9414823fdb4934b9
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-02 20:12:54 +0200

    added howtoModels

---
+++ src/models/howtoModel.ts
@@ -0,0 +1,26 @@
+import rawHowtos from '../data/itemtype_howtos.json';
+import * as qstr from '../qtools/qstr';
+
+export const buildHowtos = () => {
+	let _howtos: IHowto[] = [];
+	for (const rawHowto of rawHowtos) {
+		const howto: IHowto = {
+			id: rawHowto.id,
+			category: rawHowto.category,
+			title: rawHowto.title,
+			body: rawHowto.body,
+			bodyHtml: qstr.buildOutlineHtml(rawHowto.body),
+			bodyDescription: qstr.extractDescriptionFromOutline(rawHowto.body),
+			systemWhenCreated: rawHowto.systemWhenCreated,
+			selectedForSearch: false,
+			styledTitle: rawHowto.title,
+			styledCategory: rawHowto.category
+		};
+		_howtos.push(howto);
+	}
+
+	_howtos.sort((a, b) =>
+		a.systemWhenCreated < b.systemWhenCreated ? 1 : -1
+	);
+	return _howtos;
+}
--- src/models/model.ts
+++ src/models/model.ts
@@ -1,32 +1,9 @@
-import rawHowtos from '../data/itemtype_howtos.json';
 import rawForays from '../data/itemtype_forays.json';
+import { buildHowtos } from './howtoModel';
 import * as qstr from '../qtools/qstr';
 
-const buildHowtos = () => {
-	let _howtos: IHowto[] = [];
-	for (const rawHowto of rawHowtos) {
-		const howto: IHowto = {
-			id: rawHowto.id,
-			category: rawHowto.category,
-			title: rawHowto.title,
-			body: rawHowto.body,
-			bodyHtml: qstr.buildOutlineHtml(rawHowto.body),
-			bodyDescription: qstr.extractDescriptionFromOutline(rawHowto.body),
-			systemWhenCreated: rawHowto.systemWhenCreated,
-			selectedForSearch: false,
-			styledTitle: rawHowto.title,
-			styledCategory: rawHowto.category
-		};
-		_howtos.push(howto);
-	}
 
-	_howtos.sort((a, b) =>
-		a.systemWhenCreated < b.systemWhenCreated ? 1 : -1
-	);
-	return _howtos;
-}
-
-const buildForays = () => {
+export const buildForays = () => {
 	let _forays: IForay[] = [];
 	for (const rawForay of rawForays) {
 		const foray: IForay = {
