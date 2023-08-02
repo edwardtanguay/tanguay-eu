7bf020569a14c74162f6efce2195f1f4a134bfa3
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-03 00:30:23 +0200

    new build

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const appVersion = () => {
 	return '0.067 added siteMode'
 }
 
-export const siteMode = () => {
+export const siteMode = () => { 
 	const url = 'nnn'; // = String(window.location);
 	return url.startsWith('http://localhost') ? 'development' : 'production';
 }
