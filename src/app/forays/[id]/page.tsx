8999b1b2e3a11853aee2ec37d3efc21e1d729c3f
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-23 01:39:11 +0100

    foray pages faster

--- src/app/forays/[id]/page.tsx
+++ src/app/forays/[id]/page.tsx
@@ -1,24 +1,25 @@
+'use client';
 import * as appModel from '../../../models/model';
 import * as qdat from '../../../qtools/qdat';
 import '../../outline.scss';
 
 let siteMode = process.env.SITE_MODE;
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-   const { id } = params;
-   const foray = appModel.forays.find(m => String(m.id) === id);
-   const title = foray === undefined ? 'UNKNOWN' : `FORAY: ${foray.title}`;
-   return {
-      title,
-      openGraph: {
-         title,
-         description: foray?.bodyDescription,
-         images: ['https://tanguay-eu.vercel.app/images/siteIcon.png']
-      }
-   }
-}
+// export async function generateMetadata({ params }: { params: { id: string } }) {
+//    const { id } = params;
+//    const foray = appModel.forays.find(m => String(m.id) === id);
+//    const title = foray === undefined ? 'UNKNOWN' : `FORAY: ${foray.title}`;
+//    return {
+//       title,
+//       openGraph: {
+//          title,
+//          description: foray?.bodyDescription,
+//          images: ['https://tanguay-eu.vercel.app/images/siteIcon.png']
+//       }
+//    }
+// }
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
    const { id } = params;
    const foray = appModel.forays.find(m => String(m.id) === id);
 
--- src/app/forays/page.tsx
+++ src/app/forays/page.tsx
@@ -1,17 +1,18 @@
+'use client';
 /* eslint-disable react/no-unescaped-entities */
 import ForaySearch from '@/components/ForaySearch';
 import forays from '../../data/itemtype_forays.json';
 import { Metadata } from 'next'
 
 const pageTitle = "Edward's Forays";
-export const metadata: Metadata = {
-	title: pageTitle,
-	openGraph: {
-		title: pageTitle,
-		description: `These are my ${forays.length} forays available by search.`,
-		images: ['https://tanguay-eu.vercel.app/images/howtos.png']
-	}
-};
+// export const metadata: Metadata = {
+// 	title: pageTitle,
+// 	openGraph: {
+// 		title: pageTitle,
+// 		description: `These are my ${forays.length} forays available by search.`,
+// 		images: ['https://tanguay-eu.vercel.app/images/howtos.png']
+// 	}
+// };
 
 export default function Howtos() {
 	return (
