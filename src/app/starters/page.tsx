19a7f69e3a1aa1796e20a4c262d7841c2aa24c49
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-20 23:08:34 +0100

    corner blank

--- src/app/starters/page.tsx
+++ src/app/starters/page.tsx
@@ -3,6 +3,11 @@ export default function Starters() {
 	return (
 		<>
 			<div className="startersArea">
+				<div className="blank"></div>
+				<div className="col2">Showcases</div>
+				<div className="col3">Templates</div>
+				<div className="col4">Datapods</div>
+
 				<div className="col1">Nuxt</div>
 				<div className="col2">nnn</div>
 				<div className="col3">nnn</div>
