6d5c61ec6f166633cdc1359dc60d3c3dc16d1648
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-20 23:30:31 +0100

    all in

--- src/app/starters/page.tsx
+++ src/app/starters/page.tsx
@@ -6,26 +6,106 @@ export default function Starters() {
 				<div className="blank"></div>
 				<div className="header">
 					<div className="title">Showcases</div>
-					<div className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sunt omnis consequatur numquam quam consequuntur</div>
+					<div className="subtitle">Projects created while learning the basics of a technology, useful for returning to find basic code that works.</div>
 				</div>
 				<div className="header">
 					<div className="title">Templates</div>
-					<div className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sunt omnis consequatur numquam quam consequuntur</div>
+					<div className="subtitle">Base projects that are good starters for projects, e.g. have basic settings and tools set up for specific purposes.</div>
 				</div>
 				<div className="header">
 					<div className="title">Datapods</div>
-					<div className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium sunt omnis consequatur numquam quam consequuntur</div>
+					<div className="subtitle">Datapod is a framework for creating schemas that automatically create a CRUD user experience to manage these items.</div>
 				</div>
 
+				<div className="col1">React</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
 				<div className="col1">Nuxt</div>
-				<div className="col2">nnn</div>
-				<div className="col3">nnn</div>
-				<div className="col4">nnn</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Vue</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
 
 				<div className="col1">Go</div>
-				<div className="col2">nnn</div>
-				<div className="col3">nnn</div>
-				<div className="col4">nnn</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Next.js</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Python</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Angular 17</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Svelte</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">SvelteKit</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Electron</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">React Native</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Flutter</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Astro</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Solid</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Qwik</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Pheonix</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Laravel</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
+
+				<div className="col1">Flask</div>
+				<div className="col2"></div>
+				<div className="col3"></div>
+				<div className="col4"></div>
 			</div>
 
 		</>
