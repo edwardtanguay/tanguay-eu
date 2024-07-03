7bc9ba38561729bef2c09cc533073c9a4c36a428
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-03 19:34:55 +0200

    graphql in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,8 @@ export default function Home() {
 				<ProjectListing url="https://template-vue3-typescript-tailwind-router.vercel.app" title="Vue 3 starter template with TypeScript, Tailwind, Router, Vitest Unit-Testing" github="https://github.com/edwardtanguay/template-vue3-typescript-tailwind-router" />
 				<ProjectListing url="https://github.com/edwardtanguay/vue3-showcase-site" title="vue3-showcase-site with list/single book pages, 404 page, JSON file datasource" />
 				<ProjectListing url="https://github.com/edwardtanguay/go-showcase-examples" title="go-showcase-examples" />
+				<ProjectListing url="https://github.com/edwardtanguay/et002-graphql-backend" title="GraphQL Showcase - backend" />
+				<ProjectListing url="https://github.com/edwardtanguay/et002-graphql-frontend" title="GraphQL Showcase - frontend" />
 			</ul>
 
 			<h3 className="text-xl mb-3 text-yellow-100">Learning Resources</h3>
