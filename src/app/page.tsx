6309381dd8eb6ab11d6bd802c04280f658d16179
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-06 16:47:01 +0100

    api node/express

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,6 +171,7 @@ export default function Home() {
 				<ProjectListing url="https://github.com/edwardtanguay/vite-react-menu-createbrowserrouter-context" title="Vite-React Site with React Router 6.4 createBrowserRouter + Context" />
 				<ProjectListing url="https://github.com/edwardtanguay/vite-react-menu-createbrowserrouter-easypeasy" title="Vite-React Site with React Router 6.4 createBrowserRouter + easy-peasy Redux" />
 				<ProjectListing url="https://github.com/edwardtanguay/vite-react-node-express-lowdb-json-fullstack" title="Vite React, Node/Express, lowdb/json Fullstack Site" />
+				<ProjectListing url="https://github.com/edwardtanguay/showcase-api-node-express-ts" title="Minimal Node/Express backend with routes" />
 				<ProjectListing url="https://github.com/edwardtanguay/template-api-node-express-typescript-es6-modules-lowdb" title="Node/Express API Starter Template" />
 				<ProjectListing url="https://github.com/edwardtanguay/template-node-express-ejs-website-typescript/tree/dev" title="Node/Express EJS Website Template with TypeScript" />
 				<ProjectListing url="https://github.com/edwardtanguay/template-simple-react-with-node-express-api-one-directory" title="Vite-React with simple Node/Express API in one directory" />
